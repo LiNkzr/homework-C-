2cccb42667fb340f9eb54ec8adf390486c78e3af
TartaricAcid943 <37165741+TartaricAcid943@users.noreply.github.com>
2018-03-22 08:36:06 +0800

    Add files via upload

---
+++ 找最大值及其下标.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define N 10
+int fun(int *a,int *b,int n);
+int main()
+{ int a[N],i,max,p=0;
+for(i=0;i<N;i++) scanf("%d",&a[i]);
+ max=fun(a,&p,N);
+ printf("max=%d,position=%d\n",max,p);
+ return 0;
+}
+
+
+/* 请在这里填写答案 */
+int fun(int *a,int *b,int n)
+{
+  int num;
+  int maxium=a[0];
+  for(num=1;num<n;num++)
+  {
+    if(maxium<a[num])
+      {
+        maxium=a[num];
+        *b=num;
+      }
+  }
+  return maxium;
+}
